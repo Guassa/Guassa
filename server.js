cbbd3aa3346e5488c184d48f1c7ab55ef32ff1ed
Javojav <javipelema@gmail.com>
2017-06-27 15:13:53 +0200

    public folder was unnecessary

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(process.env.PORT);
+var server = app.listen(3000);
 
-app.use(express.static('public'));
+//app.use(express.static('public'));
 
 console.log('Server is running');
 
